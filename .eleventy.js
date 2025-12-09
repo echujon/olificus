cb660e3e11ac32d99e68d336bad9413563460efa
Efrain Chu-Jon <fchujon@hotmail.com>
2025-12-09 01:08:53 -0700

    center images

--- .eleventy.js
+++ .eleventy.js
@@ -2,5 +2,16 @@ module.exports = function(eleventyConfig) {
     // This tells Eleventy to copy the entire 'images' folder as-is to the output
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("admin");
+    return {
+      dir: {
+        input: ".",
+        output: "_site",
+        // The includes path is no longer relative to `input`
+        // Eleventy will now look in the root for your includes folder
+        includes: "_includes"
+      },
+      templateFormats: ["html", "md", "njk"],
+      htmlTemplateEngine: "njk",
+    };
   };
   
